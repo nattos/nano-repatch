0cde850549d705e92d844394eaf718b49cf8aa9f
nattos <natnyo@gmail.com>
2025-12-11 10:25:13 +0900

    Re-enable Resolume.

--- src/workers/executor.worker.ts
+++ src/workers/executor.worker.ts
@@ -59,11 +59,11 @@ self.onmessage = (event: MessageEvent<ExecutorWorkerMessage>) => {
       }
 
       // Lazy connect resolume
-      // resolumeManager.connect();
+      resolumeManager.connect();
 
       // Reset audio context ONLY on new graph load (not recompilation)
       if (!msg.isRecompilation) {
-          virtualAudioContext.reset();
+        virtualAudioContext.reset();
       }
       // Send inferred types back to main thread
       // We can just use the ones we received if we wanted to echo, but GraphExecutor stores them now.
